a856b00d8285e68daf8f7f0a2ad5a9ad7cbac34c
Calvin Comstock-Fisher <89202988+Calvin-Comstock-Fisher@users.noreply.github.com>
2021-08-23 11:55:35 -0500

    Created a rainbow poem java file.

---
+++ RainBow-Poem.java
@@ -0,0 +1,14 @@
+//This a poem about rainbows.
+//Bruh.
+//Why am I writing about Rainbows?
+//IDK.
+//goodbye.
+
+//Purple Pink Blue Orange Red
+//These are the colors of the rainbow.
+
+public class RainBowPoem {
+  public static void main(String args[]) {
+    System.out.println("rainbows :)");
+  }
+}
