f1c4bd26865454ea6338f6130f2b1f336388a565
Calvin Comstock-Fisher <89202988+Calvin-Comstock-Fisher@users.noreply.github.com>
2021-08-23 11:56:21 -0500

    Added blue

--- RainBow-Poem.java
+++ RainBow-Poem.java
@@ -11,5 +11,6 @@ public class RainBowPoem {
   public static void main(String args[]) {
     System.out.println("rainbows :)");
     System.out.println("Yellow");
+    System.out.println("Blue");
   }
 }
